89bcd2216ba175b525651eed2f13fcbe0d543881
Adesh <adeshdance@gmail.com>
2023-05-14 11:11:32 +0530

    UI design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Typewriter from "typewriter-effect";
+import MouseIcon from '@mui/icons-material/Mouse';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import logo from './page_logo.png';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="NaviDiv">
+          <img src={logo} alt='web logo' className="App-logo" />
+          <div className='navibar'>
+            <ul>
+              <li>
+                <a href='#prevention'>
+                  <span class='box'>
+                    Prevention
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href='#predict'>
+                  <span class="box">
+                    Predict
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div> 
+        <h2 className='TitleBefore'>This is about: </h2>
+        <div className='TypeWord'>
+          <Typewriter onInit={(typewriter)=> {
+              typewriter
+              .typeString("MELANOMA CANCER PREDICTION")
+              .pauseFor(1000)
+              .deleteAll()
+              .typeString("Find Your Stage")
+              .pauseFor(1000)
+              .deleteAll()
+              .typeString("Know The Preventions")
+              .pauseFor(1000)
+              .deleteAll()
+              .typeString("MELANOMA CANCER PREDICTION")
+              .start();
+              }}
+            />
+        </div>
+        <div className='JumpInTag'>
+          <p>Jump Right In </p>
+          <div><MouseIcon /></div>
+        </div>
+        <div className='Socials'>
+          <InstagramIcon style={{fontSize:'larger', marginBottom: '.3em'}}/>
+          <LinkedInIcon style={{fontSize:'larger', marginBottom: '.3em'}}/>
+          <WhatsAppIcon style={{fontSize:'larger', marginBottom: '.3em'}}/>
+          <TwitterIcon style={{fontSize:'larger', marginBottom: '.3em'}}/>
+        </div>
       </header>
+      <div className='PredictSpace' id='predict'>
+        <div className='PredictSpace-BorderDiv'>
+          <div className='PredictSpace-GradientDiv'></div>
+        </div>
+        <div className='PredictionFolder'>
+          <h2>What is Melanoma ? </h2>
+          <p className='PredictionFolder-TextArea'>Melanoma is a type of skin cancer that develops from melanocytes, 
+            the cells that produce the pigment melanin, which gives color to the skin, 
+            hair, and eyes. Melanoma is considered the most serious type of skin cancer because 
+            it can spread quickly to other parts of the body, including the lymph nodes and organs</p>
+            <hr />
+        </div>
+      </div>
     </div>
   );
 }
